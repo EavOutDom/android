5559a78fcf729d64d0d4686ba73b3a1c66157cc1
Eav Outdom <zando@ZANDOs-Mac-mini.local>
2024-12-27 10:38:36 +0700

    broadcast, foreground service and content provider

--- app/src/main/java/com/example/androidbasic/MainActivity.kt
+++ app/src/main/java/com/example/androidbasic/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.androidbasic
 
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,49 +26,70 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
+import coil3.compose.AsyncImage
 import com.example.androidbasic.ui.theme.AndroidBasicTheme
 
 class MainActivity : ComponentActivity() {
     private val airplaneModeReceiver = AirplaneModeReceiver()
+    private val imageViewModel by viewModels<ImageViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        registerReceiver(airplaneModeReceiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 0)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.POST_NOTIFICATIONS,
+                    android.Manifest.permission.READ_MEDIA_IMAGES
+                ),
+                0
+            )
         }
 
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME
+        )
+
+        val selection = "${MediaStore.Images.Media.DATE_TAKEN} >= ?"
+        val selectionArgs = arrayOf(
+            "${System.currentTimeMillis() - 1000 * 60 * 60 * 24}"
+        )
+        val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+
+        applicationContext.contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+            val images = mutableListOf<Image>()
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val contentUri = Uri.withAppendedPath(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id.toString()
+                )
+                images.add(Image(id, name, contentUri))
+            }
+            imageViewModel.updateImages(images)
+        }
         setContent {
             AndroidBasicTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-                Column (
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-
-                    Button( onClick = {
-                        Intent(applicationContext, RunningService::class.java).also {
-                            it.action = RunningService.ServiceState.STARTED.toString()
-                            startService(it)
+                    items(imageViewModel.images) { image ->
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            AsyncImage(model = image.uri, contentDescription = null)
+                            Text(text = image.name)
                         }
-                    }) {
-                        Text(text = "Start Service")
                     }
-                    Button( onClick = {
-                        Intent(applicationContext, RunningService::class.java).also {
-                            it.action = RunningService.ServiceState.STOPPED.toString()
-                            startService(it)
-                        }
-                    }) {
-                        Text(text = "Stop Service")
-                    }
-
                 }
             }
         }
@@ -72,6 +100,12 @@ class MainActivity : ComponentActivity() {
 //        unregisterReceiver(airplaneModeReceiver)
     }
 }
+
+data class Image(
+    val id: Long,
+    val name: String,
+    val uri: Uri
+)
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
